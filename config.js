4a4222ff26b0dd861e5bf1522d51d5190d5ac7ec
Lionel Ringenbach <lionel@ucodia.space>
2021-03-05 14:36:29 -0800

    Make external embeds more resilient

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
   prefix: "/",
   scope: ["easy-peasy"],
-  commands: ["ping", "cromch"],
+  commands: ["ping", "cromch", "puppies"],
 };
--- utils/createRedditCommand.js
+++ utils/createRedditCommand.js
@@ -7,11 +7,12 @@ module.exports = (sub) =>
     const top = await reddit.getTop(sub);
     const hot = await reddit.getHot(sub);
     const post = randomOf([...top, ...hot]).data;
+    const imageUrl = post.is_reddit_media_domain ? post.url : post.thumbnail;
 
     const embed = new discord.MessageEmbed()
       .setURL(`https://www.reddit.com${post.permalink}`)
       .setTitle(post.title)
-      .setImage(post.url)
+      .setImage(imageUrl)
       .setFooter(`Provided by ${post.subreddit_name_prefixed}`);
 
     message.channel.send(embed);
