9615a193dc174110d8367cf9b5179c6061091432
Lionel Ringenbach <lionel@ucodia.space>
2021-03-10 19:55:33 -0800

    Remove hug command

--- commands/index.js
+++ commands/index.js
@@ -1,11 +1,9 @@
 const ping = require("./ping");
 const breathe = require("./breathe");
-const hug = require("./hug");
 const reddit = require("./reddit");
 
 module.exports = {
   ping,
   breathe,
-  hug,
   ...reddit,
 };
