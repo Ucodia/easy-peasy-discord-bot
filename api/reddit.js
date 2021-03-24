8b9a3675bf7307ed3b83ea05da6d094c87e6a582
Lionel Ringenbach <lionel@ucodia.space>
2021-03-24 16:18:18 -0700

    Make Reddit more random

--- api/reddit.js
+++ api/reddit.js
@@ -1,27 +1,25 @@
 const axios = require("axios");
 
-const BASE_URI= "https://www.reddit.com/r"
-const unwrapResponse = (response) => response.data.data.children
+const BASE_URI = "https://www.reddit.com/r";
+const unwrapResponse = (response) => response.data.data.children;
 
-const getTop = async subreddit => {
+const getTop = async (subreddit) => {
   const response = await axios.get(
     `${BASE_URI}/${subreddit}/top.json?t=all&limit=100`
   );
   return unwrapResponse(response);
 };
 
-const getHot = async subreddit => {
+const getHot = async (subreddit) => {
   const response = await axios.get(
     `${BASE_URI}/${subreddit}/hot.json?t=all&limit=100`
   );
   return unwrapResponse(response);
 };
 
-const getRandom = async subreddit => {
-  const response = await axios.get(
-    `${BASE_URI}/${subreddit}/random.json?`
-  );
-  return unwrapResponse(response)[0];
+const getRandom = async (subreddit) => {
+  const response = await axios.get(`${BASE_URI}/${subreddit}/random.json?`);
+  return response.data[0].data.children[0];
 };
 
 module.exports = { getTop, getHot, getRandom };
--- utils/createRedditCommand.js
+++ utils/createRedditCommand.js
@@ -3,9 +3,10 @@ const reddit = require("../api/reddit");
 const { randomOf } = require("./random");
 
 module.exports = (sub) => async (message) => {
-  const top = await reddit.getTop(sub);
-  const hot = await reddit.getHot(sub);
-  const post = randomOf([...top, ...hot]).data;
+  // const top = await reddit.getTop(sub);
+  // const hot = await reddit.getHot(sub);
+  // const post = randomOf([...top, ...hot]).data;
+  const { data: post } = await reddit.getRandom(sub);
   const imageUrl = post.is_reddit_media_domain ? post.url : post.thumbnail;
 
   const embed = new discord.MessageEmbed()
