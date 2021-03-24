f91b862d82418e5ef9d888e0131ec89532cb5fe3
Lionel Ringenbach <lionel@ucodia.space>
2021-03-24 16:30:41 -0700

    Revert "Adding catnip"
    This reverts commit 808cdee990f642c0b8700cc8ee5b8b16618c3c9a.

--- commands/reddit.js
+++ commands/reddit.js
@@ -3,5 +3,4 @@ const createRedditCommand = require("../utils/createRedditCommand");
 module.exports = {
   cromch: createRedditCommand("cromch"),
   puppies: createRedditCommand("puppies"),
-  catnip: createRedditCommand("catnip"),
 };
