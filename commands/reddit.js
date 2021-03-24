808cdee990f642c0b8700cc8ee5b8b16618c3c9a
Lionel Ringenbach <lionel@ucodia.space>
2021-03-24 16:27:19 -0700

    Adding catnip

--- commands/reddit.js
+++ commands/reddit.js
@@ -3,4 +3,5 @@ const createRedditCommand = require("../utils/createRedditCommand");
 module.exports = {
   cromch: createRedditCommand("cromch"),
   puppies: createRedditCommand("puppies"),
+  catnip: createRedditCommand("catnip"),
 };
