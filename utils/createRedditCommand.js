680c3aa46050c220c8c11baa53c49ed77fd92dfd
Lionel Ringenbach <ucodia@live.com>
2021-10-14 12:48:18 -0700

    [refactor] Pick Reddit result from Top and Hot

--- utils/createRedditCommand.js
+++ utils/createRedditCommand.js
@@ -3,11 +3,17 @@ const reddit = require("../api/reddit");
 const { randomOf } = require("./random");
 
 module.exports = (sub) => async (message) => {
-  // const top = await reddit.getTop(sub);
-  // const hot = await reddit.getHot(sub);
-  // const post = randomOf([...top, ...hot]).data;
-  const post = (await reddit.getRandom(sub)).data;
-  const imageUrl = post.is_reddit_media_domain ? post.url : post.thumbnail;
+  const top = await reddit.getTop(sub);
+  const hot = await reddit.getHot(sub);
+  // const post = (await reddit.getRandom(sub)).data;
+
+  let post = null;
+  let imageUrl = "";
+
+  do {
+    post = randomOf([...top, ...hot]).data;
+    imageUrl = post.is_reddit_media_domain ? post.url : post.thumbnail;
+  } while (!imageUrl.startsWith("http"));
 
   const embed = new discord.MessageEmbed()
     .setURL(`https://www.reddit.com${post.permalink}`)
