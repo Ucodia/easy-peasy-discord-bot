45b2157449b265792502214fac1c1016cb828c37
Lionel Ringenbach <ucodia@live.com>
2021-10-14 12:47:15 -0700

    [feat] Add Reddit API caching

--- api/reddit.js
+++ api/reddit.js
@@ -1,24 +1,32 @@
 const axios = require("axios");
+const AxiosCacheAdapter = require("axios-cache-adapter");
+
+const cache = AxiosCacheAdapter.setupCache({
+  maxAge: 15 * 60 * 1000,
+});
+const api = axios.create({
+  adapter: cache.adapter,
+});
 
 const BASE_URI = "https://www.reddit.com/r";
 const unwrapResponse = (response) => response.data.data.children;
 
 const getTop = async (subreddit) => {
-  const response = await axios.get(
+  const response = await api.get(
     `${BASE_URI}/${subreddit}/top.json?t=all&limit=100`
   );
   return unwrapResponse(response);
 };
 
 const getHot = async (subreddit) => {
-  const response = await axios.get(
+  const response = await api.get(
     `${BASE_URI}/${subreddit}/hot.json?t=all&limit=100`
   );
   return unwrapResponse(response);
 };
 
 const getRandom = async (subreddit) => {
-  const response = await axios.get(`${BASE_URI}/${subreddit}/random.json?`);
+  const response = await api.get(`${BASE_URI}/${subreddit}/random.json?`);
   return response.data[0].data.children[0];
 };
 
